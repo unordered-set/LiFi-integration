b878235f1238b14b7902e36db5a28992a51cd296
Kostyan <unordered_set@tutanota.com>
2023-05-13 15:41:39 +0000

    Attempt 2

---
+++ lifi2/app/lifi.jsx
@@ -0,0 +1,24 @@
+'use client'
+
+import dynamic from 'next/dynamic';
+
+const LoadingIndicator = () => {
+    return (
+      <div
+        style={{
+          display: 'grid',
+          placeItems: 'center',
+        }}
+      >
+        <p>Loading...</p>
+      </div>
+    );
+  };
+
+export const LiFiWidgetNext = dynamic(
+    () => import('./widget').then((module) => module.Widget),
+    {
+        ssr: false,
+        loading: () => <LoadingIndicator />,
+    },
+);
