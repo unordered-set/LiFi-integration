9e082d5dbc43e37a4c22d68dcb8b5336f17dcfd9
Node Run <snipkaa@gmail.com>
2023-05-21 09:54:53 +0000

    Menu

--- lifi2/app/page.js
+++ lifi2/app/page.js
@@ -3,9 +3,10 @@ import { LiFiWidgetNext } from './lifi'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello, HareCrypta!
-      <LiFiWidgetNext /> 
+    <main style={{backgrounde : '#0d112e', padding : '10rem'}}>
+      <sectione>
+          <LiFiWidgetNext /> 
+      </sectione>
     </main>
   )
 }
