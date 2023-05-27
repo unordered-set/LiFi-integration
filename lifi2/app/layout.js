510a39e2303bb4421d30c3e29f2e95102ecc8c06
Node Run <snipkaa@gmail.com>
2023-05-27 07:11:01 +0000

    style

--- lifi2/app/layout.js
+++ lifi2/app/layout.js
@@ -3,10 +3,7 @@ import './css/bootstrap-min.css'
 import './css/style.css'
 import './css/rootcss.css'
 import './css/globals.css'
-import { Inter } from 'next/font/google'
 
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'HareCrypta Exchange | Открытое криптовалютное комьюнити ',
