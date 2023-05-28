76602f7e1c047f229a498ba11f4c3a1d265ba32d
Node Run <snipkaa@gmail.com>
2023-05-28 17:19:49 +0000

    style

--- lifi2/app/layout.js
+++ lifi2/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body data-rsssl="1" class="home page-template page-template-page-templates page-template-template-onepage page-template-page-templatestemplate-onepage-php page page-id-395 translatepress-ru_RU wpb-js-composer js-comp-ver-6.9.0 vc_responsive elementor-default elementor-kit-4 elementor-page elementor-page-395 ico-dark icos-theme-ver-1.4.8 wordpress-version-6.0.5 &quot; data-spy=&quot;scroll&quot; data-target=&quot;#mainnav&quot; data-offset=&quot;80 no-touch loaded" data-offset="97.1406">
+      <body>
         {children}
       </body>
     </html>
