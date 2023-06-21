0fa220ba56d14cf8717e7a7d8c47c9f78f861e8b
Node Run <snipkaa@gmail.com>
2023-06-21 19:52:50 +0000

    Font

--- lifi2/app/page.js
+++ lifi2/app/page.js
@@ -9,8 +9,14 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Image from 'react-bootstrap/Image'
 import { Play } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 
 const play = Play({
+  subsets: ['cyrillic', 'latin'],
+  weight: ['400', '700'],
+})
+
+const montserrat = Montserrat({
   subsets: ['cyrillic', 'latin'],
   weight: ['400', '700'],
 })
@@ -20,7 +26,7 @@ export default function Home() {
   return (
     <div className="wrapped">
       <header className="site-header">
-        <Navbar className="is-transparent" collapseOnSelect variant="dark" expand="lg">
+        <Navbar className={`is-transparent ${montserrat.className}`} collapseOnSelect variant="dark" expand="lg">
           <Container>
             <Navbar.Brand>
               <Image className="logo" src="https://harecrypta.com/wp-content/uploads/2022/09/logo-footer-1.png" alt="harecrypta.com" /> </Navbar.Brand>
@@ -64,7 +70,7 @@ export default function Home() {
             <div id="block-4" className="widget footer-widget widget_block widget_media_image">
               <figure className="wp-block-image size-full"><img loading="lazy" width="287" height="82" src="https://harecrypta.com/wp-content/uploads/2022/09/logo-footer-1.png" alt="" className="wp-image-1344" /></figure>
             </div>
-            <div id="block-6" className="widget footer-widget widget_block widget_text" style={{display: 'block', fontSize: 14}}>
+            <div id="block-6" className={`widget footer-widget widget_block widget_text ${montserrat.className}`} style={{display: 'block', fontSize: 14}}>
               <p>Вы еще не с нами в соцсетях? Горячие минты и свежие инсайты тут! Следите за нами в соц. сетях!</p>
             </div>
             <div className="social telegram">
@@ -107,7 +113,7 @@ export default function Home() {
             <div id="nav_menu-3" className="widget footer-widget widget_nav_menu">
               <h6 className={`widget-title ${play.className}`}>Навигация</h6>
               <div className="menu-futer-menyu-2-container">
-                <Nav id="menu-futer-menyu-2" className="menu" style={{display: 'block', fontSize: 14}}>
+                <Nav id="menu-futer-menyu-2" className={`menu ${montserrat.className}`} style={{display: 'block', fontSize: 14}}>
                   <Nav.Link id="menu-item-2795" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-395 current_page_item menu-item-2806" href="https://harecrypta.com/">Главная</Nav.Link>
                   <Nav.Link id="menu-item-2795" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-2813" href="https://harecrypta.com/konsalting/">Консалтинг</Nav.Link>
                   <Nav.Link id="menu-item-2795" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-2807" href="https://harecrypta.com/news-harecrypta/">Новости HareCrypta</Nav.Link>
@@ -123,7 +129,7 @@ export default function Home() {
             <div id="nav_menu-3" className="widget footer-widget widget_nav_menu">
               <h6 className={`widget-title ${play.className}`}>Важное HareCrypta</h6>
               <div className="menu-futer-menyu-container">
-                <Nav id="menu-futer-menyu" className="menu" style={{display: 'block', fontSize: 14}}>
+                <Nav id="menu-futer-menyu" className={`menu ${montserrat.className}`} style={{display: 'block', fontSize: 14}}>
                   <Nav.Link id="menu-item-2798" className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-2798" href="https://harecrypta.com/category/statya/">Статья HareCrypta</Nav.Link>
                   <Nav.Link id="menu-item-2795" className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-2795" href="https://harecrypta.com/category/vyzhimki/">Выжимки HareCrypta</Nav.Link>
                   <Nav.Link id="menu-item-2796" className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-2796" href="https://harecrypta.com/category/makroekonomika/">Макроэкономика</Nav.Link>
@@ -136,7 +142,7 @@ export default function Home() {
             <div id="nav_menu-4" className="widget footer-widget widget_nav_menu">
               <h6 className={`widget-title ${play.className}`}>Информация</h6>
               <div className="menu-informacziya-container">
-                <Nav id="menu-informacziya" className="menu" style={{display: 'block', fontSize: 14}}>
+                <Nav id="menu-informacziya" className={`menu ${montserrat.className}`} style={{display: 'block', fontSize: 14}}>
                   <Nav.Link id="menu-item-1170" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-1170" href="https://harecrypta.com/category/statya/">Статьи HareCrypta</Nav.Link>
                   <Nav.Link id="menu-item-1168" className="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-1168" href="https://harecrypta.com" >GitBook про NFT (coming soon)</Nav.Link>
                   <Nav.Link id="menu-item-1315" className="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-1315" href="https://harecrypta.com/">Инструменты (coming soon)</Nav.Link>
@@ -150,7 +156,7 @@ export default function Home() {
       <footer className="section footer-section section-pad-nopd section-bg ">
         <Container>
           <Row className="text-center">
-            <Col md={12}> <span className="copyright-text"> Copyright © 2022 by <a href="/">HareCrypta</a></span> </Col>
+            <Col md={12}> <span className={`copyright-text ${montserrat.className}`}> Copyright © 2022 by <a href="/">HareCrypta</a></span> </Col>
           </Row>
         </Container>
       </footer>
